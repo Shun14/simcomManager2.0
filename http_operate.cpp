4447983d67873d094c40d9dd8dd2a982aa96bc1b
Shun14 <zzsshun13@gmail.com>
2017-05-12 16:08:43 +0800

    调整ui,下一步添加ui

--- http_operate.cpp
+++ http_operate.cpp
@@ -1,8 +1,9 @@
-#include "http_operate.h"
+﻿#include "http_operate.h"
 
 #include <QtNetwork>
 #include <QMessageBox>
 #include <QApplication>
+#include <QObject>
 
 http_operate::http_operate(){
     return;
@@ -52,10 +53,10 @@ QString http_operate::httpOperarte(const QString &url, const QString &data, cons
 
     QString _result;
     if(_reply->error() != QNetworkReply::NoError){
-        QMessageBox::information(parent, QString("小安提示"), QString("%1\r\n访问失败：%2\r\n").arg(url).arg(_reply->error()));
+        QMessageBox::information(parent, QObject::tr("xiaoantishi"), QObject::tr("%1\r\n connect failed: %2\r\n").arg(url).arg(_reply->error()));
     }
     else if(_timeout){
-        QMessageBox::information(parent, QString("小安提示"), QString("%1\r\n无响应\r\n").arg(url));
+        QMessageBox::information(parent, QObject::tr("xiaoantishi"), QObject::tr("%1\r\n no reponse\r\n").arg(url));
     }
     else {
         _result = _reply->readAll();
@@ -117,10 +118,10 @@ QString http_operate::httpsOperarteLeancloud(const QString &url, const QString &
 
     QString _result;
     if(_reply->error() != QNetworkReply::NoError){
-        QMessageBox::information(parent, QString("小安提示"), QString("%1\r\n访问失败：%2\r\n").arg(url).arg(_reply->error()));
+        QMessageBox::information(parent, QObject::tr("xiaoantishi"), QObject::tr("%1\r\n connect failed: %2\r\n").arg(url).arg(_reply->error()));
     }
     else if(_timeout){
-        QMessageBox::information(parent, QString("小安提示"), QString("%1\r\n无响应\r\n").arg(url));
+        QMessageBox::information(parent, QObject::tr("xiaoantishi"), QObject::tr("%1\r\n no reponse\r\n").arg(url));
     }
     else {
         _result = _reply->readAll();
@@ -129,4 +130,63 @@ QString http_operate::httpsOperarteLeancloud(const QString &url, const QString &
     _reply->deleteLater();
 
     return _result;
+}
+
+QString http_operate::httpOperarteWithAuth(const QString &url, const QString &data, const QString &type, QString &username, QString &passwd, QWidget *parent) {
+
+    QNetworkRequest _req;
+
+
+    QSslConfiguration _sslCon = _req.sslConfiguration();
+    _sslCon.setPeerVerifyMode(QSslSocket::VerifyNone);
+    _req.setSslConfiguration(_sslCon);
+
+    _req.setUrl(QUrl(url));
+    _req.setHeader(QNetworkRequest::ContentTypeHeader, QString("application/json"));
+
+    QString concatenated = username.toLatin1()+":"+passwd.toLatin1();
+    QByteArray authData = concatenated.toLocal8Bit().toBase64();
+
+    QString headerData = "Basic " + authData;
+
+    _req.setRawHeader("Authorization", headerData.toLocal8Bit());
+
+    QNetworkAccessManager authManager;
+    QNetworkReply  * _reply;
+    if ( type == "GET") {
+        _reply = authManager.get(_req);
+    }
+    if ( type == "POST") {
+        _reply = authManager.post(_req,data.toLatin1());
+    }
+    _reply->ignoreSslErrors();
+
+    QTime _t;
+    _t.start();
+
+    int TIMEOUT = (10 * 1000);
+    bool _timeout = false;
+
+    while (!_reply->isFinished()){
+        QApplication::processEvents();
+        if (_t.elapsed() >= TIMEOUT) {
+            _timeout = true;
+            break;
+        }
+    }
+
+    QString _result;
+    if(_reply->error() != QNetworkReply::NoError){
+        QMessageBox::information(parent, QObject::tr("xiaoantishi"), QObject::tr("%1\r\n connect failed: %2\r\n").arg(url).arg(_reply->error()));
+    }
+    else if(_timeout){
+        QMessageBox::information(parent, QObject::tr("xiaoantishi"), QObject::tr("%1\r\n no reponse\r\n").arg(url));
+    }
+    else {
+        _result = _reply->readAll();
+    }
+    _reply->deleteLater();
+
+    return _result;
+
 }
--- http_operate.h
+++ http_operate.h
@@ -1,4 +1,4 @@
-#ifndef HTTP_OPERATE_H
+﻿#ifndef HTTP_OPERATE_H
 #define HTTP_OPERATE_H
 
 #include <QMainWindow>
@@ -18,7 +18,7 @@ public:
 public:
     QString httpOperarte(const QString &url, const QString &data, const QString &type, QWidget *parent);
     QString httpsOperarteLeancloud(const QString &url, const QString &data, const QString &type, QWidget *parent);
-
+    QString httpOperarteWithAuth(const QString &url, const QString &data, const QString &type, QString &username, QString &passwd,QWidget *parent);
 };
 
 #endif // HTTP_OPERATE_H
